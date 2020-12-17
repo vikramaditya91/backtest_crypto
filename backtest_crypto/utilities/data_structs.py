aef5fdf81848752b878a2c6277038287ddfa8e6c
vikramaditya <vikramaditya91@gmail.com>
2020-12-17 23:25:59 +0100

    Fix Type hints

--- backtest_crypto/utilities/data_structs.py
+++ backtest_crypto/utilities/data_structs.py
@@ -1,8 +1,14 @@
+from __future__ import annotations
 import pandas as pd
 import xarray as xr
+from typing import TYPE_CHECKING, Dict
+if TYPE_CHECKING:
+    from backtest_crypto.utilities.iterators import TimeIntervalIterator
 
-def time_interval_iterator_to_pd_multiindex(time_interval_iterator,
-                                            ):
+
+def time_interval_iterator_to_pd_multiindex(
+        time_interval_iterator: TimeIntervalIterator,
+    ) -> pd.MultiIndex:
     time_intervals = time_interval_iterator.time_intervals
     start_list, end_list = zip(*time_intervals)
     return pd.MultiIndex.from_product([list(set(start_list)),
@@ -11,6 +17,6 @@ def time_interval_iterator_to_pd_multiindex(time_interval_iterator,
                                              'end_time'])
 
 
-def coordinates_to_dict(dataset: xr.Dataset):
+def coordinates_to_dict(dataset: xr.Dataset) -> Dict:
     coords = dataset.to_dict()["coords"]
-    return dict(map(lambda x:(x[0], x[1]["data"]), coords.items()))
+    return dict(map(lambda x: (x[0], x[1]["data"]), coords.items()))
